933a3adb5a45a1f15c01836de02764a443bda7a4
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2025-08-24 03:35:33 +0000

    add english translate in Quran each surah, English translate is from Sah

--- src/components/shared/VerseCard.tsx
+++ src/components/shared/VerseCard.tsx
@@ -1,20 +1,30 @@
+'use client';
+
 import { Card, CardContent } from '@/components/ui/card';
+import { Separator } from '../ui/separator';
 
 interface VerseCardProps {
   number: string;
   arabic: string;
   lao: string;
+  english: string;
 }
 
-export default function VerseCard({ number, arabic, lao }: VerseCardProps) {
+export default function VerseCard({ number, arabic, lao, english }: VerseCardProps) {
   return (
     <Card className="shadow-sm mb-4">
-      <CardContent className="p-4">
-        <div className="flex justify-between items-start mb-2 text-right">
+      <CardContent className="p-4 space-y-4">
+        <div className="flex justify-between items-start text-right">
           <span className="text-sm font-bold text-primary mr-2 leading-loose">{`(${number})`}</span>
           <p className="font-arabic text-2xl leading-relaxed text-foreground">{arabic}</p>
         </div>
-        <p className="text-lg font-bold text-card-foreground mt-4 leading-relaxed">{lao}</p>
+        <div>
+          <p className="text-lg font-bold text-card-foreground leading-relaxed">{lao}</p>
+        </div>
+        <Separator />
+        <div>
+          <p className="text-md text-muted-foreground leading-relaxed italic">"{english}"</p>
+        </div>
       </CardContent>
     </Card>
   );
